e8f1c075e04b9294e36724f5ccbb1964dc242dcc
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2021-05-04 14:55:10 +0900

    project-2 첫 이슈 시작

---
+++ pro.gg/src/main/java/Project/pro/gg/Configuration/DBConfiguration.java
@@ -0,0 +1,55 @@
+package Project.pro.gg.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DBConfiguration {
+
+    /*
+     * DataSource 를 hikariConfig 에서 설정한 정보(applicatoin.properties)로 만들어준다.
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
+
+    /*
+     * 위에서 설정한 datasource 를 사용하고 쿼리가 작성되는 xml 위치를 지정해 줌으로서 추후 Mapper or DAO 레벨에서 사용되는
+     * 쿼리르 인식해주는 과정, 여기서 classpath 는 src/main/resources 이다.
+     */
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
