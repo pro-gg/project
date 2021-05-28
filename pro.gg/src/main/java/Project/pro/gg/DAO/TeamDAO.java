8928fc790b1468362330fc64269634824d357df9
HoJun-Seo <ghwns6659@gmail.com>
2021-05-29 00:03:47 +0900

    project-24 팀 신청 및 신청 수락, 거절 기능 구현 1. 회원 - 소환사 테이블간 데이터 저장 필드 및 매핑 구조 변경 2. 팀 목록, 팀 생성, 팀 상세 기능 테스트 코드 작성 및 테스트 통과

--- pro.gg/src/main/java/Project/pro/gg/DAO/TeamDAO.java
+++ pro.gg/src/main/java/Project/pro/gg/DAO/TeamDAO.java
@@ -1,5 +1,6 @@
 package Project.pro.gg.DAO;
 
+import Project.pro.gg.Model.MemberDTO;
 import Project.pro.gg.Model.TeamDTO;
 
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 public interface TeamDAO {
     public TeamDTO selectTeam(TeamDTO teamDTO);
 
-    public void insertTeamData(TeamDTO teamDTO);
+    public void insertTeamData(TeamDTO teamDTO, MemberDTO memberDTO);
 
     public List<TeamDTO> selectTeamList();
 }
