8c6c36aa6b1be868ee530a6cd4003fa1bfd7e55c
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2021-06-04 13:15:46 +0900

    project-22 팀 생성 기능 구현

--- pro.gg/src/main/java/Project/pro/gg/Controller/TeamController.java
+++ pro.gg/src/main/java/Project/pro/gg/Controller/TeamController.java
@@ -3,7 +3,6 @@ package Project.pro.gg.Controller;
 import Project.pro.gg.Model.MemberDTO;
 import Project.pro.gg.Model.TeamDTO;
 import Project.pro.gg.Service.TeamServiceImpl;
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -36,6 +34,7 @@ public class TeamController {
             teamDTO.setTeam_description((String) jsonObject.getString("team_description"));
 
             teamDTO.setCaptinName((String) jsonObject.getString("captinName"));
+            teamDTO.setTop((String) jsonObject.getString("captinName"));
             memberDTO.setUserid((String) jsonObject.getString("userid"));
             memberDTO.setTeamName(teamDTO.getTeamName());
         }catch (Exception e){
