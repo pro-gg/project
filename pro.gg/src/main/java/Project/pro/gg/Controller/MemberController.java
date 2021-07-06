f4421d8c03a41ff242cdcfac05d1ad01109917d8
jin12352 <54969395+jin12352@users.noreply.github.com>
2021-07-07 04:02:26 +0900

    네이버 카카오 로그인

--- pro.gg/src/main/java/Project/pro/gg/Controller/MemberController.java
+++ pro.gg/src/main/java/Project/pro/gg/Controller/MemberController.java
@@ -1,22 +1,37 @@
 package Project.pro.gg.Controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import Project.pro.gg.Model.*;
-import Project.pro.gg.Service.SummonerServiceImpl;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import Project.pro.gg.API.KakaoAPI;
+import Project.pro.gg.Model.AdminDTO;
+import Project.pro.gg.Model.MemberDTO;
+import Project.pro.gg.Model.RankedFlexDTO;
+import Project.pro.gg.Model.RankedSoloDTO;
+import Project.pro.gg.Model.SummonerDTO;
 import Project.pro.gg.Service.MemberServiceImpl;
+import Project.pro.gg.Service.SummonerServiceImpl;
 
 @Controller
 public class MemberController {
@@ -28,6 +43,8 @@ public class MemberController {
     SummonerServiceImpl summonerService;
 
     public static HttpSession session;
+    
+    KakaoAPI kakaoApi = new KakaoAPI();
 
     @PostMapping("/check_id.do")
     public void check_id(HttpServletRequest request, HttpServletResponse response) {
@@ -280,5 +297,123 @@ public class MemberController {
         model.addAttribute("ranked_solo", rankedSoloDTO);
         model.addAttribute("ranked_flex", rankedFlexDTO);
         return "searchNickNameResult";
+    }
+    @GetMapping("/naver.do")
+    public String naverLogin(@RequestParam("code") String code, @RequestParam("state") String state) throws UnsupportedEncodingException {
+    	String clientId = "_GlAhgDzVIlPh0a5FTYm";
+		String clientSecret = "3bQFANR1Il"; 
+		String redirectURI = URLEncoder.encode("/main.jsp","UTF-8");
+				
+		String apiURL = "";
+		apiURL += "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+		apiURL += "client_id=" + clientId;
+		apiURL += "&client_secret=" + clientSecret;
+		apiURL += "&redirect_uri=" + redirectURI;
+		apiURL += "&code=" + code;
+		apiURL += "&state=" + state;
+		String access_token = "";
+		String refresh_token = ""; //나중에 이용
+		
+		try { 
+			  URL naverUrl = new URL(apiURL);
+		      HttpURLConnection con = (HttpURLConnection)naverUrl.openConnection();
+		      con.setRequestMethod("GET");
+		      int responseCode = con.getResponseCode();
+		      BufferedReader br;
+		      if(responseCode==200) { // 정상 호출
+		        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		      } else {  // 에러 발생
+		        br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		      }
+		      String inputLine;
+		      StringBuffer res = new StringBuffer();
+		      while ((inputLine = br.readLine()) != null) {
+		        res.append(inputLine);
+		      }
+		      br.close();
+		      if(responseCode==200) {
+//		    	  System.out.println(res.toString());
+		    		JSONParser parsing = new JSONParser();
+		    		org.json.simple.JSONObject result = (org.json.simple.JSONObject)parsing.parse(res.toString());
+		    		
+		    		
+		    		access_token = (String)result.get("access_token");
+		    		refresh_token = (String)result.get("refresh_token");
+		    		if(access_token != null) {
+		    			try {
+		    				 String header = "Bearer " + access_token;
+		    				 String apiurl = "https://openapi.naver.com/v1/nid/me";
+		    				 naverUrl = new URL(apiurl);
+		    				 con = (HttpURLConnection)naverUrl.openConnection();
+		    				con.setRequestMethod("GET");
+		    				con.setRequestProperty("Authorization", header);
+		    				responseCode = con.getResponseCode();
+		    				if(responseCode==200) { // 정상 호출
+		    				 br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		    				} else {  // 에러 발생
+		    				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		    				}
+		    				inputLine = "";
+		    				res = new StringBuffer();
+		    				 while ((inputLine = br.readLine()) != null) {
+		    				res.append(inputLine);
+		    				}
+		    				br.close();
+		    				
+		    				parsing = new JSONParser();
+		    				result = (org.json.simple.JSONObject)parsing.parse(res.toString());
+		    				
+		    				String id = (String)((org.json.simple.JSONObject)result.get("response")).get("id");
+		    				String email = (String)((org.json.simple.JSONObject)result.get("response")).get("email");
+		    				String name = (String)((org.json.simple.JSONObject)result.get("response")).get("name");
+		    				String nickname = (String)((org.json.simple.JSONObject)result.get("response")).get("nickname");
+		    				
+		    				System.out.println(id);
+		    				MemberDTO memberDTO = memberService.selectMemberOne(id);
+		    				if(memberDTO == null) {
+		    					memberDTO = new MemberDTO();
+		    	    			memberDTO.setUserid(id);
+		    	    			memberDTO.setPasswd(id);
+		    	    			memberDTO.setName(name);
+		    	    			memberDTO.setNickname(nickname);
+		    	    			memberDTO.setEmail(email);
+		    	    			memberService.insert(memberDTO);
+		    	    		}
+		    				
+		    				session.setAttribute("member", memberDTO);
+						} catch (Exception e) {
+							
+						}
+		    		}
+		      }
+		    } catch (Exception e) {
+		      System.out.println(e);
+		    }
+    	
+    	return "main";
+    }
+    
+    @GetMapping("/kakao.do")
+    public String login(@RequestParam("code") String code, HttpServletRequest request) {
+    	String access_token = kakaoApi.getAccessToken(code);
+    	HashMap<String, String> userInfo = kakaoApi.getUserInfo(access_token);
+    	MemberDTO memberDTO = new MemberDTO();
+    	if(userInfo.get("id") != null) {
+    		memberDTO = memberService.selectMemberOne(userInfo.get("id"));
+    		if(memberDTO == null) {
+    			memberDTO = new MemberDTO();
+    			memberDTO.setUserid(userInfo.get("id"));
+    			memberDTO.setPasswd(userInfo.get("id"));
+    			memberDTO.setNickname(userInfo.get("nickname"));
+    			memberDTO.setName(userInfo.get("nickname"));
+    			memberDTO.setEmail(userInfo.get("email"));
+    			
+    			memberService.insert(memberDTO);
+    		}
+    	}
+    	session = request.getSession();
+    	session.setAttribute("member", memberDTO);
+    	
+    	return "main";
     }
 }
